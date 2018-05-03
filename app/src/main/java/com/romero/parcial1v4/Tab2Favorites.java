e0ccc01b77fa39cf4b33234d736ab440ba566a41
Sara <00030716@uca.edu.sv>
2018-05-02 20:22:21 -0600

    Initial commit

---
+++ app/src/main/java/com/romero/parcial1v4/Tab2Favorites.java
@@ -0,0 +1,17 @@
+package com.romero.parcial1v4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Favorites extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2favorites, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/romero/parcial1v4/Tab3Search.java
@@ -0,0 +1,17 @@
+package com.romero.parcial1v4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Search extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3search, container, false);
+        return rootView;
+    }
+}
