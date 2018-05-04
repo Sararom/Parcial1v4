ed633427c2e6ab10f6b64cbf9001de43a23203f5
Sara <00030716@uca.edu.sv>
2018-05-03 22:51:58 -0600

    Se agregó un dialog que muestra la informacion del contacto y se agregaron dos botones al item

--- app/src/main/java/com/romero/parcial1v4/RecyclerViewAdapter.java
+++ app/src/main/java/com/romero/parcial1v4/RecyclerViewAdapter.java
@@ -1,13 +1,18 @@
 package com.romero.parcial1v4;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,6 +20,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     Context mContext;
     List<Contact> mData;
+    Dialog myDialog;
 
     public RecyclerViewAdapter(Context mContext, List<Contact> mData) {
         this.mContext = mContext;
@@ -27,7 +33,30 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         View v;
         v= LayoutInflater.from(mContext).inflate(R.layout.item_contact,parent,false);
-        MyViewHolder vHolder = new MyViewHolder(v);
+        final MyViewHolder vHolder = new MyViewHolder(v);
+
+        //Inicializacion del dialogo
+
+        myDialog= new Dialog(mContext);
+        myDialog.setContentView(R.layout.dialog_contact);
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        vHolder.item_contact.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                TextView dialog_name_tv = (TextView) myDialog.findViewById(R.id.dialog_name_id);
+                TextView dialog_phone_tv = (TextView) myDialog.findViewById(R.id.dialog_phone_id);
+                ImageView dialog_img_iv = (ImageView) myDialog.findViewById(R.id.dialog_img_id);
+                dialog_name_tv.setText(mData.get(vHolder.getAdapterPosition()).getName());
+                dialog_phone_tv.setText(mData.get(vHolder.getAdapterPosition()).getPhone());
+                dialog_img_iv.setImageResource(mData.get(vHolder.getAdapterPosition()).getPhoto());
+
+                Toast.makeText(mContext,"Test Click "+String.valueOf(vHolder.getAdapterPosition()),Toast.LENGTH_SHORT).show();
+                myDialog.show();
+            }
+        });
+
         return vHolder;
     }
 
@@ -47,6 +76,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
+        private LinearLayout item_contact;
         private TextView tv_name;
         private TextView tv_phone;
         private ImageView img;
@@ -54,6 +84,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public MyViewHolder(View itemView) {
             super(itemView);
 
+            item_contact = (LinearLayout) itemView.findViewById(R.id.contact_item_id);
             tv_name = (TextView) itemView.findViewById(R.id.contact_name);
             tv_phone = (TextView) itemView.findViewById(R.id.phone_number);
             img = (ImageView) itemView.findViewById(R.id.img_contact);
--- app/src/main/java/com/romero/parcial1v4/Tab1Contacts.java
+++ app/src/main/java/com/romero/parcial1v4/Tab1Contacts.java
@@ -20,7 +20,10 @@ public class Tab1Contacts extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Se agrega el recycleview a la vista de la tab de contactos
         View rootView = inflater.inflate(R.layout.tab1contacts, container, false);
+
         myRecyclerView = (RecyclerView) rootView.findViewById(R.id.contact_recycler);
         RecyclerViewAdapter recyclerAdapter = new RecyclerViewAdapter(getContext(),lstContacts);
         myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -31,12 +34,13 @@ public class Tab1Contacts extends Fragment {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         lstContacts = new ArrayList<>();
-        lstContacts.add(new Contact("Desgraciado 1","22222222",R.drawable.contact_pp));
-        lstContacts.add(new Contact("Desgraciado 2","88888888",R.drawable.contact_pp));
-        lstContacts.add(new Contact("Desgraciado 3","77777777",R.drawable.contact_pp));
+        lstContacts.add(new Contact("Desgraciado 1","22222222",R.drawable.contact_pp_pop));
+        lstContacts.add(new Contact("Desgraciado 2","88888888",R.drawable.contact_pp_pop));
+        lstContacts.add(new Contact("Desgraciado 3","77777777",R.drawable.contact_pp_pop));
 
     }
 }
